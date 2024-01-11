485a3d4d33ccad50004a2171b5927541c02cdda2
EmaSmolic <ema.smolic@fer.hr>
2024-01-11 22:48:13 +0100

    .

--- gamePack.ts
+++ gamePack.ts
@@ -61,7 +61,7 @@ export abstract class Game {
 
     this.clientSocket.connect()
     //register as a game at env server
-    this.clientSocket.emit('hello_from_game',this.clientSocket, )
+    this.clientSocket.emit('hello_from_game', 'some_data' )
   }
 
   public getSocket(): Socket<DefaultEventsMap, DefaultEventsMap> { return this.clientSocket }
