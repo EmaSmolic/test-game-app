b1c05e4f30e33cdc5105294edb1b86020fb6e800
EmaSmolic <ema.smolic@fer.hr>
2024-01-17 13:56:11 +0100

    .

--- Classes.ts
+++ Classes.ts
@@ -74,9 +74,12 @@ export class Environment {
 
       socket.on('message', (message: any, id: (string | null)) => {
 console.log('MESSAGE ON ENV', message, id)
+console.log(this.ctrlrs_ids)
         for (const ctrlr in this.ctrlrs_ids.keys()) {
           console.log(ctrlr)
           //check if sent to that id (include all if id is null) AND if this ctrlr socket corresponds to this RCA socket
+          console.log(this.ctrlrs_rcas)
+
           if ((!id || this.ctrlrs_ids.get(ctrlr) == id) && this.ctrlrs_rcas.get(ctrlr) == socket.id)
             this.server.sockets.in(ctrlr).emit('message', message)
         }
