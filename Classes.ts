7873a3fe5e56be9715ab8591e7db638292ddff99
EmaSmolic <ema.smolic@fer.hr>
2024-01-17 13:19:45 +0100

    .

--- Classes.ts
+++ Classes.ts
@@ -73,7 +73,7 @@ export class Environment {
       })
 
       socket.on('message', (message: any, id: (string | undefined)) => {
-
+console.log('MESSAGE ON ENV', message, id)
         for (const ctrlr in this.ctrlrs_ids.keys()) {
           //check if sent to that id (include all if id is undefined) AND if this ctrlr socket corresponds to this RCA socket
           if ((!id || this.ctrlrs_ids.get(ctrlr) == id) && this.ctrlrs_rcas.get(ctrlr) == socket.id)
