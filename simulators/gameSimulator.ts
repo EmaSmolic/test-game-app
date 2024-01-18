0526068919a26438407e481a74d90cc2964e499a
EmaSmolic <ema.smolic@fer.hr>
2024-01-18 17:34:35 +0100

    .

--- simulators/gameSimulator.ts
+++ simulators/gameSimulator.ts
@@ -1,4 +1,4 @@
-import { Environment, Controller } from '../Classes';
+import { Environment, Controller } from '../../Classes';
 import { DemoController } from './DemoController';
 import { DemoGame } from './DemoGame'
 import { io as client_io } from "socket.io-client";
@@ -24,21 +24,7 @@ var socket = require('socket.io')(server, {'transports': [ 'websocket' ],})
 
 const env = new Environment(socket)
 server.listen(8000)
-const thisGame = new DemoGame("https://test-igrica.onrender.com/", "test_code")
-const thisCtrlr = new DemoController("https://test-igrica.onrender.com/")
 
-setTimeout(() => {
-  console.log('TRYING TO CONNECT')
-  thisCtrlr.tryConnecting("test_code")
-}, 10000);
-
-setTimeout(() => {
-  thisCtrlr.sendControl({type: 'some control'})
-}, 20000);
-
-setTimeout(() => {
-  thisGame.sendMessage({message: 'hello'})
-}, 30000);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, './')));
 app.get('/', function (req: any, res: any) {
